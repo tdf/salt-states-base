f84d06dd00965518b67400f34e5cd30d3872c2a2
Alexander Werner <alex@documentfoundation.org>
2013-12-06 12:56:11 +0100

    Updated testrunner

--- doc/testing.rst
+++ doc/testing.rst
@@ -6,3 +6,17 @@ Preparation
 -----------
 
 - Install `Docker <http://docker.io>`
+- On Debian Wheezy, you need to install the kernel from backports to be able to use docker
+
+Usage
+-----
+
+- Go to the directory :file:`test` in the `salt-states-base` distribution
+- Run the test suite::
+
+    sudo ./run.sh
+
+- This will do the following:
+  
+  - Download Debian 6, Debian 7, Centos 6, Ubuntu 12.04 Containers
+  - Run a local highstate with all states enabled in each of them
--- test/run.sh
+++ test/run.sh
@@ -17,4 +17,8 @@ pushd docker/centos-6
 docker build -t "salt-states-base/centos:6" .
 popd
 set +e
-docker run -rm=true -v ..:/srv/salt:ro salt-states-base/debian:7 salt-call state.highstate -l debug
+STATEPATH=$(readlink -e state)
+docker run -rm=true -v $STATEPATH:/srv/salt:ro salt-states-base/debian:7 salt-call state.highstate -l debug | tee log/debian-7.log
+docker run -rm=true -v $STATEPATH:/srv/salt:ro salt-states-base/debian:6 salt-call state.highstate -l debug | tee log/debian-6.log
+docker run -rm=true -v $STATEPATH:/srv/salt:ro salt-states-base/centos:6 salt-call state.highstate -l debug | tee log/centos-6.log
+docker run -rm=true -v $STATEPATH:/srv/salt:ro salt-states-base/ubuntu:12.04 salt-call state.highstate -l debug | tee log/ubuntu-12.04.log
