f24eefeedb3f6faa8ce8c9c35fe9a1d338c16312
Alexander Werner <alex@documentfoundation.org>
2014-04-25 12:03:14 +0000

    Added default aliases and colored ls output

--- users/Z99-user.sh
+++ users/Z99-user.sh
@@ -7,3 +7,9 @@
 alias grepconf='grep -vE "^#|^$|^[\w]+#"'
 alias grep='grep --color'
 alias sudox='sudo cp ~/.Xauthority /root && sudo '
+export LS_OPTIONS='--color=auto'
+eval "`dircolors`" 
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+force_color_prompt=yes
