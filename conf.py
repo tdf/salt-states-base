47fee71b9a22a4f8dd7b31e537d164b24481a84e
Alexander Werner <alex@documentfoundation.org>
2015-09-23 07:28:56 +0000

    Fixed copyright line

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'salt-states-base'
-copyright = u'2013, Robert Einsle, Alexander Werner'
+copyright = u'2015, The Document Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
