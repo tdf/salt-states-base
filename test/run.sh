b9a35fcdbc20507f04ba3af6fd9742b91408bcb6
Alexander Werner <alex@documentfoundation.org>
2015-12-03 21:29:09 +0100

    Parallel run.sh

--- test/run.sh
+++ test/run.sh
@@ -8,9 +8,17 @@ STATEPATH=$(readlink -e ..)
 COMMAND=${*:-state.highstate}
 LOGCOMMAND=${COMMAND// /_}
 DISTROS=(debian:7 debian:8 centos:7 ubuntu:12.04 ubuntu:14.04)
-RETCODE=0
+PID=$$
+rm -f /tmp/$PID.fail
 for DISTRO in ${DISTROS[*]}; do
+    (
     echo "Running $COMMAND on $DISTRO"
-    docker run --rm=true -v $STATEPATH:/srv/salt:ro salt_states_base/$DISTRO salt-call $COMMAND --local --retcode-passthrough -l warning --force-color || RETCODE=1
+    docker run --rm=true -v $STATEPATH:/srv/salt:ro salt_states_base/$DISTRO salt-call $COMMAND --local --retcode-passthrough -l warning --force-color || touch /tmp/$PID.fail
+    ) &
 done
-exit $RETCODE
+wait
+if [ -f /tmp/$PID.fail ]; then
+    rm -f /tmp/$PID.fail
+    exit 1
+fi
+exit 0
