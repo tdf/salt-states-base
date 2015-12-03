972489a41e2dbaab76c3a4679b0b9f8aa1520f13
Alexander Werner <alex@documentfoundation.org>
2015-12-03 17:59:33 +0100

    Macke docker builds silent

--- test/build.sh
+++ test/build.sh
@@ -4,8 +4,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 set -e
-docker build -t "salt_states_base/debian:7" docker/debian-7
-docker build -t "salt_states_base/debian:8" docker/debian-8
-docker build -t "salt_states_base/ubuntu:12.04" docker/ubuntu-12.04
-docker build -t "salt_states_base/ubuntu:14.04" docker/ubuntu-14.04
-docker build -t "salt_states_base/centos:7" docker/centos-7
+docker build -q -t "salt_states_base/debian:7" docker/debian-7
+docker build -q -t "salt_states_base/debian:8" docker/debian-8
+docker build -q -t "salt_states_base/ubuntu:12.04" docker/ubuntu-12.04
+docker build -q -t "salt_states_base/ubuntu:14.04" docker/ubuntu-14.04
+docker build -q -t "salt_states_base/centos:7" docker/centos-7
