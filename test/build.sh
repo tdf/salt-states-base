2afeeca3a7264c02a8f3cfcf316bb7ae2bd166d9
Alexander Werner <alex@documentfoundation.org>
2015-12-03 21:36:53 +0100

    Parallel docker build

--- test/build.sh
+++ test/build.sh
@@ -4,8 +4,9 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 set -e
-docker build -q -t "salt_states_base/debian:7" docker/debian-7
-docker build -q -t "salt_states_base/debian:8" docker/debian-8
-docker build -q -t "salt_states_base/ubuntu:12.04" docker/ubuntu-12.04
-docker build -q -t "salt_states_base/ubuntu:14.04" docker/ubuntu-14.04
-docker build -q -t "salt_states_base/centos:7" docker/centos-7
+docker build -q -t "salt_states_base/debian:7" docker/debian-7 &
+docker build -q -t "salt_states_base/debian:8" docker/debian-8 &
+docker build -q -t "salt_states_base/ubuntu:12.04" docker/ubuntu-12.04 &
+docker build -q -t "salt_states_base/ubuntu:14.04" docker/ubuntu-14.04 &
+docker build -q -t "salt_states_base/centos:7" docker/centos-7 &
+wait
